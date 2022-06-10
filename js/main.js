f2f86f31a5618d811112b1acf5688c3dc7420b23
DmitryS73 <dmitriy.stepanov73@gmail.com>
2022-06-10 21:05:47 +0300

    Добавляет функции

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let getRandomNumber = function (number1, number2) {
+  if (number2 < number1) {
+    return Math.floor(Math.random()*(number1 - number2 + 1)) + number2;
+  }
+  if (number2 === number1) {
+    return number2;
+  }
+  return Math.floor(Math.random()*(number2 - number1 + 1)) + number1;
+}
+
+let checkStringLength = function (string, maxLength) {
+  if (string.length > maxLength) {
+    return true;
+  }
+  return false;
+}
