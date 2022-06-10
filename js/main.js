9f0bfcf3af3bacbcadf3dbb2e7209687892b39ef
DmitryS73 <dmitriy.stepanov73@gmail.com>
2022-06-11 00:08:30 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const getRandomNumber = function (number1, number2) {
   if (number1 >= 0 && number2 >=0) {
     if (number2 < number1) {
-      return Math.floor(Math.random()*(number1 - number2 + 1)) + number2;
+    return Math.floor(Math.random()*(number1 - number2 + 1)) + number2;
     }
     if (number2 === number1) {
-      return number2;
-      }
-      return Math.floor(Math.random()*(number2 - number1 + 1)) + number1;
+    return number2;
+    }
+    return Math.floor(Math.random()*(number2 - number1 + 1)) + number1;
     } else {
-      console.log('Неверный диапазон');
+    console.log('Неверный диапазон');
   }
 };
 
 const checkStringLength = function (string, maxLength) {
   if (string.length <= maxLength) {
-    return true;
+  return true;
   }
   return false;
 };
